3fc328eb64b43caaf7afef767312df8b9c713f59
Raga Mohammad <ragaaidil8@gmail.com>
2026-09-10 11:57:19 +0700

    Add files via upload

---
+++ pekan 2/Numerik_2611532002.py
@@ -0,0 +1,13 @@
+# Buat file dengan nama Numerik_2611532002.py
+# nama variabel ditambah 4 digit terakhir NIM contoh: a_5500
+# create a variabel with numeric type value
+a_2002=100
+print("The type of variabel having value",a_2002,"is",type(a_2002))
+
+# create a variabel with float value
+b_2002=10.2345
+print("The type of variabel having value",b_2002,"is",type(b_2002))
+
+# create a variabel with complex value
+c_2002=100+3j
+print("The type of variabel having value",c_2002,"is",type(c_2002))
